3ad70b842b4a0faa231c078d1877b60faeb9f680
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-04-02 09:48:57 +0200

    upload solution

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('funguju!');
+/*Doplňte do stránky JavaScriptový program, který pomocí funkce prompt požádá uživatele aby vyjářil svoji úroveň postupně ve všech zobrazených dovednostech jako číslo 0–100. Program pak nastaví posuvníky na stránce na odpovídající délku a hodnoty u posuvníků na uživatelem zadaná čísla.
+Na samém začátku vašeho programu vytvořte funkci updateSkill, která bere dva vstupy: textový řetězec představující id elementu a číslo mezi 0–100. Tato funkce při zavolání nastaví posuvník se zadaným id na správnou hodnotu. Použijte tuto funkci ve vašem kódu, abyste nemuseli při nastavování posuvníků psát třikrát po sobě téměř stejný kód.*/
+
+
+const updateSkill = (skillID, value) => {
+  const progressBar = document.querySelector(`${skillID} .skill__progress`);
+  const progressValue = document.querySelector(`${skillID} .skill__value`);
+  progressBar.style.width = `${value}%`;
+  progressValue.textContent = `${value}/100`;
+}
+
+
+const htmlProgress = Number(prompt('jaka je vase uroven HTML?'))
+const cssProgress = Number(prompt('jaka je vase uroven CSS?'))
+const jsProgress = Number(prompt('jaka je vase uroven JavaScriptu?'))
+
+updateSkill('#skill1', htmlProgress);
+updateSkill('#skill2', cssProgress);
+updateSkill('#skill3', jsProgress);
